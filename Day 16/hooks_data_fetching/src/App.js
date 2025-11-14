739a8b1b0854808fd22cc8f22ca46f4371b9ae35
Abner-Hilks <staright1920guy@gmail.com>
2025-11-14 10:00:30 +0530

    Day 16 is pushed

---
+++ Day 16/hooks_data_fetching/src/App.js
@@ -0,0 +1,25 @@
+// Here we will call the hooks demo component
+import React from 'react';  
+
+import { useFetch } from "./component/useFetch";
+
+function App() {
+  const data = useFetch('https://jsonplaceholder.typicode.com/posts');
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="App">
+      <h1>Hooks Demo - Fetching Data</h1>
+      {data.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+      {/* <HooksDemo posts={data} /> */}
+    </div>
+  );
+}
+  
+export default App;
